868477dbe88bf1e2af7acd583b9d84dc68ed37d2
Javier330 <andreumatasjavi@gmail.com>
2024-11-14 22:07:49 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Este archivo sigue igual porque la funcionalidad no cambia.
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = '';
+}
+
+function calculateResult() {
+    try {
+        let display = document.getElementById("display");
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
