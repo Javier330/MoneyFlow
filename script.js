ac77717d96e6179f6f8075453550320afd683565
Javier330 <andreumatasjavi@gmail.com>
2024-11-16 17:29:39 +0100

    script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// script.js
-
-// Ejecutar el código después de que el DOM esté completamente cargado
-document.addEventListener('DOMContentLoaded', () => {
-    // Función para manejar el desplazamiento suave a las secciones de la página
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault(); // Evita el comportamiento predeterminado
-            const target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-                target.scrollIntoView({
-                    behavior: 'smooth', // Desplazamiento suave
-                    block: 'start'     // Alinea con la parte superior
-                });
-            }
-        });
+// Función para manejar el desplazamiento suave a las secciones de la página
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        if (target) {
+            target.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
     });
+});
 
-    // Manejador para los botones de suscripción en los planes
-    document.querySelectorAll('.plans .cta-button').forEach(button => {
-        button.addEventListener('click', function () {
-            window.location.href = 'https://javier330.github.io/registro/'; // Redirige a una página específica
-        });
+// Manejador para los botones de suscripción
+document.querySelectorAll('.cta-button.subscribe').forEach(button => {
+    button.addEventListener('click', function () {
+        alert('¡Gracias por suscribirte!');
+    });
+});
+
+// Manejador para los botones de prueba gratuita
+document.querySelectorAll('.cta-button.free-trial').forEach(button => {
+    button.addEventListener('click', function () {
+        alert('¡Disfruta tu prueba gratuita de 30 días!');
     });
 });
