315c788e7f20d12273d7243906370960354181f6
Javier330 <andreumatasjavi@gmail.com>
2024-11-16 16:53:47 +0100

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 // script.js
 
-// Función para manejar el desplazamiento suave a las secciones de la página
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if (target) {
-            target.scrollIntoView({
-                behavior: 'smooth'
-            });
-        }
+// Ejecutar el código después de que el DOM esté completamente cargado
+document.addEventListener('DOMContentLoaded', () => {
+    // Función para manejar el desplazamiento suave a las secciones de la página
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault(); // Evita el comportamiento predeterminado
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth', // Desplazamiento suave
+                    block: 'start'     // Alinea con la parte superior
+                });
+            }
+        });
     });
-});
 
-// Manejador para los botones de suscripción en los planes
-document.querySelectorAll('.plans .cta-button').forEach(button => {
-    button.addEventListener('click', function () {
-        window.location.href = 'https://javier330.github.io/registro/'; // Redirige a una página específica
-        // Aquí podrías añadir lógica adicional o redireccionamiento
+    // Manejador para los botones de suscripción en los planes
+    document.querySelectorAll('.plans .cta-button').forEach(button => {
+        button.addEventListener('click', function () {
+            window.location.href = 'https://javier330.github.io/registro/'; // Redirige a una página específica
+        });
     });
 });
